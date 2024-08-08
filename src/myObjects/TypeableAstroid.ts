3009c093a16373d267571e635e6efd2b48a4907e
phlipzamo <phlipzamo14@gmail.com>
2024-08-07 23:19:00 -0700

    group of typeable objects that fall

--- src/myObjects/TypeableAstroid.ts
+++ src/myObjects/TypeableAstroid.ts
@@ -1,22 +1,26 @@
 import { TypeableText } from "./TypeableText";
 
-export class TypeableAstroid{
+export class TypeableAstroid extends Phaser.GameObjects.GameObject{
     typeableText: TypeableText;
     astroid: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    startLetter: string;
     beingTyped: boolean;
     x: number;
     y: number;
+    anims: any;
    
     constructor(scene:Phaser.Scene,x:number, y:number,text:string,fontsize:number){
+        super(scene, 'typeableAstroid');
         this.astroid = scene.physics.add.sprite(10,10,"astroid");
         this.typeableText = new TypeableText(scene,x,y,text,fontsize);
+        this.startLetter = text[0];
         this.beingTyped= false;
     }
     moveDown(speed:number){
         this.move(this.x, this.y+speed)
     }
     move(x:number,y:number){
-        this.x =x;
+        this.x = x;
         this.y = y;
         this.typeableText.moveTo(x,y);
         this.astroidMove(x,y);
@@ -24,5 +28,12 @@ export class TypeableAstroid{
     astroidMove(x:number, y:number){
         this.astroid.x =x;
         this.astroid.y =y;
+    }
+    setBeingTyped(bool:boolean){
+        this.beingTyped= bool;
+    }
+    setVisible(bool:boolean){
+        this.astroid.setVisible(bool);
+        this.typeableText.setVisible(bool);
     }
 }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -10,6 +10,7 @@ export class Game extends Scene
     aGrid: AlignGrid;
     astroid: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     typeableAstroid: TypeableAstroid;
+    groupOfAstroids: Phaser.GameObjects.Group;
 
     constructor ()
     {
@@ -18,18 +19,69 @@ export class Game extends Scene
 
     create ()
     {
-        this.aGrid = new AlignGrid(this, 5,5)
+        this.aGrid = new AlignGrid(this, 5,5);
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x000000);
-        this.typeableAstroid= new TypeableAstroid(this, 0,0,"hello",30)
-        var indexPos = this.aGrid.getPosByIndex(2);
-        this.typeableAstroid.move(indexPos.x, indexPos.y);
-        //this.astroid=this.physics.add.sprite(10,10,"astroid");
-        //this.aGrid.placeAtIndex(2,this.astroid);
-        this.earth = this.add.image(0, 0, 'earth');
+        this.createAndPlaceEarth()
+        
+        this.createAnimations();
+        this.groupOfAstroids = this.add.group();
+        this.groupOfAstroids.add(this.createAndPlaceTypeableAstroid("hello", 2));
+        this.groupOfAstroids.add(this.createAndPlaceTypeableAstroid("josie", 1));
+        
+
+        if(!this.input.keyboard){return}
+        this.input.keyboard.on('keydown', (keyPressed:any) => {
+            const typeableAstroid = <TypeableAstroid> this.groupOfAstroids.getChildren()
+                .find((child)=>{
+                    var typeableAstroid:TypeableAstroid= <TypeableAstroid> child;
+                    return typeableAstroid.beingTyped===true;
+                }
+            );
+            if(typeableAstroid){
+                if(!typeableAstroid.typeableText.getFirstAlive()){return}
+                if(typeableAstroid.typeableText.getFirstAlive().text===keyPressed.key){
+                    typeableAstroid.typeableText.getFirstAlive().setTyped(true);
+                    if(!typeableAstroid.typeableText.getFirstAlive()){
+                        typeableAstroid.setVisible(false);
+                        typeableAstroid.destroy(false);
+                    }
+                }
+            }
+            else{
+                const typeableAstroid = <TypeableAstroid> this.groupOfAstroids.getChildren()
+                    .find((child)=>{
+                        var typeableAstroid:TypeableAstroid= <TypeableAstroid> child;
+                        return typeableAstroid.startLetter === keyPressed.key;
+                    }
+                );
+                if(typeableAstroid){
+                    typeableAstroid.typeableText.getFirstAlive().setTyped(true);
+                    typeableAstroid.setBeingTyped(true);
+                }
+                
+            }
+        });           
+    }
+    update() {
+        this.groupOfAstroids.getChildren().forEach((child)=>{
+            var typeableAstroid:TypeableAstroid= <TypeableAstroid> child;
+           typeableAstroid.moveDown(1);
+        });
+    }
+    createAndPlaceEarth(){
+        this.earth = this.physics.add.sprite(0, 0, 'earth');
         this.aGrid.placeAtIndex(22, this.earth);
         this.earth.setScale(19);
         this.earth.y+=this.earth.displayHeight/3;
+    }
+    createAndPlaceTypeableAstroid(text:string,index:number):TypeableAstroid{
+        var typeableAstroid= new TypeableAstroid(this, 0,0,text,30)
+        var indexPos = this.aGrid.getPosByIndex(index);
+        typeableAstroid.move(indexPos.x, indexPos.y);
+        return typeableAstroid;
+    }
+    createAnimations(){
         this.anims.create({
             key: 'stop',
             frames: this.anims.generateFrameNames('astroid', {start: 1, end: 1, zeroPad: 1, prefix: 'A', suffix: '.png'}),
@@ -42,8 +94,5 @@ export class Game extends Scene
             frameRate: 16,
             repeat: 0
         });
-    }
-    update() {
-        this.typeableAstroid.moveDown(1);
     }
 }
