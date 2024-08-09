f72cd8cf284d2dba84d98d7090996715484dbb91
phlipzamo <phlipzamo14@gmail.com>
2024-08-09 15:29:55 -0700

    Update Boot.ts

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -11,10 +11,10 @@ export class Boot extends Scene
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
-        this.load.image('background', require('assets/bg.png'));
-        this.load.image('earth', require('assets/images/globe.png'));
-        this.load.atlas("astroid", require("assets/astroid.png"), require("assets/astroid.json"));
-        this.load.json("words", require("assets/words.json"));
+        this.load.image('background', 'assets/bg.png');
+        this.load.image('earth', 'assets/images/globe.png')
+        this.load.atlas("astroid", "assets/astroid.png", "assets/astroid.json");
+        this.load.json("words", "assets/words.json");
     }
 
     create ()
