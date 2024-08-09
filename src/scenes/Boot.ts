56852bd4372641d3456a480ab31d7f50db511e63
phlipzamo <phlipzamo14@gmail.com>
2024-08-09 15:57:32 -0700

    build changes

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -12,7 +12,7 @@ export class Boot extends Scene
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
         this.load.image('background', 'assets/bg.png');
-        this.load.image('earth', 'assets/images/globe.png')
+        this.load.image('earth', 'assets/globe.png')
         this.load.atlas("astroid", "assets/astroid.png", "assets/astroid.json");
         this.load.json("words", "assets/words.json");
     }
